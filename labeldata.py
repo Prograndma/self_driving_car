36ec69b0d2561d3362b48a48630ab583ccd46127
User <thomasvcole@gmail.com>
2024-04-04 12:59:51 -0600

    WIP, trying to make this more usable.

--- labeldata.py
+++ labeldata.py
@@ -1,44 +1,54 @@
-import cv2
-import uuid
+import cv2 as cv
 import time
+import argparse
 
 prev_x = 0
 prev_y = 0
 
-def mouseCapture(event, x, y, flags, param):
+
+def mouse_capture(event, x, y, flags, param):
     global prev_x
     global prev_y
-    if event == cv2.EVENT_MOUSEMOVE:
+    if event == cv.EVENT_MOUSEMOVE:
         prev_x = x
         prev_y = y
 
 
-def main():
-    cap = cv2.VideoCapture("figure8traindata.mkv")
-    cv2.namedWindow('Capture')
-    cv2.setMouseCallback('Capture', mouseCapture)
+def parse_frames(video, label_file, delay=.01666):
+    cap = cv.VideoCapture(video)
+    cv.namedWindow('Capture')
+    cv.setMouseCallback('Capture', mouse_capture)
     time.sleep(1)
-    count = 0
-    with open("lane_labels.txt", 'w') as out_file:
+    with open(label_file, 'w') as out_file:
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 break
-            cv2.imshow("Capture", frame)
-            # Modify this line to adjust how fast the video plays back
-            time.sleep(0.01666)
-            count += 1
-            if cv2.waitKey(10) & 0xFF == ord('q'):
+            cv.imshow("Capture", frame)
+            time.sleep(delay)
+            if cv.waitKey(10) & 0xFF == ord('q'):
                 break
-            #img_name = f'lane_following/{prev_x}_0_{count}.jpg'
             img_name = str(prev_x)
-            #cv2.imwrite(img_name, frame)
-            #print(img_name)
-            out_file.write(img_name+"\n")
-
+            out_file.write(img_name + "\n")
 
     cap.release()
-    cv2.destroyAllWindows()
+    cv.destroyAllWindows()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='This will help you label a video.',
+        epilog='You\'re going to have to provide arguments.')
+
+    parser.add_argument('video', type=str, default=None, help='the relative path to your video file.')
+    parser.add_argument('out_file', type=str, default="labels.txt", help='The name of the output file')
+    parser.add_argument('frame_delay', type=float, default=.01666, help='Use this number to label faster or slower.')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                        const=sum, default=max,
+                        help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
 
 
 if __name__ == "__main__":
